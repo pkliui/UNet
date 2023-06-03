2206021441f4eab797371b53e0839a209cfe43f6
pkliui <kliuiev@pm.me>
2023-06-03 19:25:36 +0300

    tests: add dummy data to test baseloader

--- UNet/tests/test_base_loader.py
+++ UNet/tests/test_base_loader.py
@@ -53,26 +53,71 @@ class TestBaseDataLoader(unittest.TestCase):
 
     def test_read_batch(self):
         """
-        test reading batches of existing data
+        Test reading batches of existing data
+        following this file structure for PH2 data
+            root_folder
+            ├── folder_with_images
+            │    ├── sampleID1
+            │    │    ├── sampleID1_image
+            │    │    │    └── image1.bmp
+            │    │    └── sampleID1_mask
+            │    │         └── mask1.bmp
+            │    ├── sampleID2
+            │    │    ├── sampleID2_image
+            │    │    │    └── image2.bmp
+            │    │    └── sampleID2_mask
+            │    │         └── mask2.bmp
+            │    ├──...
+            ├── ...
         """
-        # set args
-        DATAPATH = os.path.abspath("/Users/Pavel/Documents/repos/UNet/docs/data/PH2_Dataset_images/")
-        images_folder = "images"
-        masks_folder = "masks"
-        extension = "*.bmp"
-        transform = None
+        num_images = 10
+
+        # Create dummy image and mask folders and files
+        sample_data = []
+        for i in range(1, num_images + 1):
+            sampleID = f"sampleID{i}"
+            image_file = f"image{i}.bmp"
+            mask_file = f"mask{i}.bmp"
+            sample_data.append({
+                "sampleID": sampleID,
+                "image_file": image_file,
+                "mask_file": mask_file
+            })
+
+            sample_folder = os.path.join(self.test_dir, sampleID)
+            os.makedirs(sample_folder)
+
+            image_folder = os.path.join(sample_folder, f"{sampleID}_image")
+            os.makedirs(image_folder)
+            image_file_path = os.path.join(image_folder, image_file)
+            with open(image_file_path, "w") as f:
+                f.write("Dummy image data")
+
+            mask_folder = os.path.join(sample_folder, f"{sampleID}_mask")
+            os.makedirs(mask_folder)
+            mask_file_path = os.path.join(mask_folder, mask_file)
+            with open(mask_file_path, "w") as f:
+                f.write("Dummy mask data")
+
+        # Create the UNetDataset with the temporary image and mask files
+        images_list = [
+            os.path.join(self.test_dir, sample["sampleID"], f"{sample['sampleID']}_image", sample["image_file"])
+            for sample in sample_data
+        ]
+        masks_list = [
+            os.path.join(self.test_dir, sample["sampleID"], f"{sample['sampleID']}_mask", sample["mask_file"])
+            for sample in sample_data
+        ]
         # read masks and images
-        unet_data = UNetDataset(extension=extension,
-                                root_dir=DATAPATH,
-                                images_folder=images_folder,
-                                masks_folder=masks_folder,
-                                transform=transform)
+        unet_data = UNetDataset(images_list=images_list,
+                                masks_list=masks_list,
+                                transform=None)
 
         #
         # set args for dataloader
-        batch_size = 1
-        validation_split = 0.25
-        test_split = 0.25
+        batch_size = 2
+        validation_split = 0.2
+        test_split = 0.2
         # create the corresponding dataloader for training and validation
         self.bdl = BaseDataLoader(dataset=unet_data,
                                      batch_size=batch_size,
@@ -81,14 +126,14 @@ class TestBaseDataLoader(unittest.TestCase):
                                   shuffle_for_split=True,
                                   random_seed_split=0)
         # check the batch size
-        self.assertEqual(self.bdl.batch_size, 1)
+        self.assertEqual(self.bdl.batch_size, 2)
         # check the validation split
-        self.assertEqual(self.bdl.validation_split, 0.25)
+        self.assertEqual(self.bdl.validation_split, 0.2)
         # check the number of batches for validation
         self.assertEqual(len(self.bdl.val_loader), 1)
         self.assertEqual(len(self.bdl.test_loader), 1)
         # check the number of batches for training
-        self.assertEqual(len(self.bdl.train_loader), 2)
+        self.assertEqual(len(self.bdl.train_loader), 3)
 
 
 
