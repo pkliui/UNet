93ebd0d32943186bf7bbb2a149d16e0293b45c4d
pkliui <kliuiev@pm.me>
2023-06-22 12:21:08 +0300

    docs: testing without furo theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -33,13 +33,6 @@ release = '1.0'
 extensions = [
 ]
 
-# Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
-
-# List of patterns, relative to source directory, that match files and
-# directories to ignore when looking for source files.
-# This patterns also effect to html_static_path and html_extra_path
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 # If true, `todo` and `todoList` produce output, else they produce nothing.
 todo_include_todos = False
@@ -49,13 +42,7 @@ todo_include_todos = False
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'furo'
-
-# Add any paths that contain custom static files (such as style sheets) here,
-# relative to this directory. They are copied after the builtin static files,
-# so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
-
+#html_theme = 'furo'
 
 # simply add the extension to your list of extensions
 extensions = ['myst_parser', "sphinx_togglebutton",]
